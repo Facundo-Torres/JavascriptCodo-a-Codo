049b469899d2b2198ec5aab76fe1bb086050c011
Facundo <78511908+Facundo-Torres@users.noreply.github.com>
2022-11-01 16:24:45 -0300

    Add files via upload

---
+++ borrar.js
@@ -0,0 +1,9 @@
+
+function btn_borrar(){
+    document.getElementById('nombre').value = '';
+    document.getElementById('apellido').value = '';
+    document.getElementById('correo').value = '';
+    document.getElementById('list').value = '';
+    document.getElementById('cantidad').value = '';
+    document.getElementById('mostrar').value = '';
+}
